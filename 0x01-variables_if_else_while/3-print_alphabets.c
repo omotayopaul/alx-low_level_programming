da879114e14965caa968cae469a00326ccc54038
omotayopaul <pauloyediran1@gmail.com>
2022-03-13 09:35:37 -0700

    my 3rd

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	char lower_case; /*Decarling statement*/
 	char upper_case;
-	
+
 	/* for lower case*/
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
 		putchar(lower_case);
 	}
-	
+
 	/*for upper case*/
 	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 	{
