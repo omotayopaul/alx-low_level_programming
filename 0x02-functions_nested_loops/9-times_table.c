e634ca445a299fc15fc81be53ff7ad7e8f0a17dd
omotayopaul <pauloyediran1@gmail.com>
2022-03-16 14:11:38 -0700

    my 9th task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,7 +28,7 @@ void times_table(void)
 				}
 				if (b < 9)
 				{
-					;_putchar(',')
+					_putchar (',')
 					_putchar (' ');
 				}
 			}
@@ -38,7 +38,7 @@ void times_table(void)
 				_putchar ((c % 10) + '0');
 				if (b < 9)
 				{
-					_putchar(',');
+					_putchar (',');
 					_putchar (' ');
 				}
 			}
