ba21b4f7c3e9b2b8e4489df21dbfa016542fc648
omotayopaul <pauloyediran1@gmail.com>
2022-03-30 20:39:06 -0700

    my commits

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,12 +14,12 @@ int _evaluate(int i, int n)
 
 	else if (i * i < n)
 		return (_evaluate(i + 1, n));
-	
+
 	else if (i * i == n) /*Condiction base*/
 		return (i);
-	
+
 	return (-1);
-	
+
 	return (-1);
 }
 
@@ -32,10 +32,10 @@ int _evaluate(int i, int n)
 int _sqrt_recursion(int n)
 {
 	int i = 0;
-	
+
 	if (i < 0) /*If n is negative*/
 		return (-1);
-	
+
 	else
 	{
 		return (_evaluate(i, n)); /*Recursive call*/
