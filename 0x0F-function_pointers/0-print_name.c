8746682d0c6994e4de7292997a8663f0712d4806
omotayopaul <pauloyediran1@gmail.com>
2022-04-13 20:02:33 -0700

    my task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints a name.
- *@name: name
- *@f: callback function
- * Description: function that prints a name
+ * print_name - Execute the function passed by argument
+ * @name: String name
+ * @f: Function void that receives as argument a string 
+ *
+ * Return: nothing;
  */
-
 void print_name(char *name, void (*f)(char *))
-
 {
-	if (name != NULL && f != NULL)
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
 	f(name);
 }
